4a65fdaeb69c4f1d48eede328e1d45e8e3b76492
thefourCraft <me@thefourcraft.com>
2024-08-17 16:07:37 +0300

    refactor: Update FAQSection, ContactSection, and ValuesSection component styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import CenteredTextWithLines from '@site/src/components/CenteredTextWithLines';
 import ValuesSection from '@site/src/components/ValuesSection';
 import ContactSection from '@site/src/components/ContactSection';
@@ -30,7 +29,6 @@ export default function Home(): JSX.Element {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
         <CenteredTextWithLines />
         <ValuesSection />
         <ContactSection />
