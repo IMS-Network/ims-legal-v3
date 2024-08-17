a19ffd9cdbe9d6f867bd6b484f44f0a353eaf9b8
thefourCraft <me@thefourcraft.com>
2024-08-17 15:26:06 +0300

    chore: Update background image URL and add parallax effect to hero banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,25 +4,20 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-import BigHero from '@site/src/components/BigHero';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero', styles.heroBanner, styles.parallaxBackground)}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
+        <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
+          <a className="button button--secondary button--lg" href="/docs/intro">
             Docusaurus Tutorial - 5min ⏱️
-          </Link>
+          </a>
         </div>
       </div>
     </header>
@@ -35,7 +30,6 @@ export default function Home(): JSX.Element {
     <Layout
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <BigHero />
       <HomepageHeader />
       <main>
         <HomepageFeatures />
